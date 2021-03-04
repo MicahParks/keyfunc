a69239dccc7b01e5a1ae3b48bb582eae91413b91
Micah Parks <git@micahparks.com>
2021-03-04 18:31:25 -0500

    Use a context you neanderthal.

--- get.go
+++ get.go
@@ -1,21 +1,29 @@
 package jwks
 
 import (
+	"bytes"
+	"context"
 	"io/ioutil"
 	"net/http"
 )
 
 // Get loads the JWKS at the given URL.
-func Get(client *http.Client, jwksURL string) (keystore Keystore, err error) {
+func Get(ctx context.Context, client *http.Client, jwksURL string) (keystore Keystore, err error) {
 
 	// Use the default HTTP client if none was given.
 	if client == nil {
 		client = http.DefaultClient
 	}
 
+	// Create the HTTP request.
+	var req *http.Request
+	if req, err = http.NewRequestWithContext(ctx, http.MethodGet, jwksURL, bytes.NewReader(nil)); err != nil {
+		return nil, err
+	}
+
 	// Get the JWKS as JSON from the given URL.
 	var resp *http.Response
-	if resp, err = client.Get(jwksURL); err != nil {
+	if resp, err = client.Do(req); err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close() // Ignore any error.
