2e46dc142ad6a5a4e566931f4c086e2e89f4fdf0
Micah Parks <git@micahparks.com>
2021-06-26 20:33:57 -0400

    More conditions cause a background goroutine.

--- options.go
+++ options.go
@@ -16,20 +16,22 @@ type Options struct {
 	RefreshErrorHandler ErrorHandler
 
 	// RefreshInterval is the duration to refresh the JWKs in the background via a new HTTP request. If this is not nil,
-	// then a background refresh will be performed in a separate goroutine until the JWKs method EndBackground is
-	// called.
+	// then a background refresh will be requested in a separate goroutine at this interval until the JWKs method
+	// EndBackground is called.
 	RefreshInterval *time.Duration
 
-	// RefreshRateLimit TODO
+	// RefreshRateLimit limits the rate at which refresh requests are granted. Only one refresh request can be reserved
+	// at a time any refresh requests received while there is already a reservation are ignored. It does not make sense
+	// to have RefreshInterval's value shorter than this.
 	RefreshRateLimit *time.Duration
 
 	// RefreshTimeout is the duration for the context used to create the HTTP request for a refresh of the JWKs. This
 	// defaults to one minute. This is only effectual if RefreshInterval is not nil.
 	RefreshTimeout *time.Duration
 
-	// RefreshUnknownKID indicates that the JWKs should be refreshed via HTTP every time a kid that isn't known is
-	// found. This means that a malicious client could self-sign X JWTs, send them to this service, then cause
-	// potentially high network usage proportional to X.
+	// RefreshUnknownKID indicates that the JWKs refresh request will occur every time a kid that isn't cached is seen.
+	// Without specifying a RefreshInterval a malicious client could self-sign X JWTs, send them to this service,
+	// then cause potentially high network usage proportional to X.
 	RefreshUnknownKID *bool
 }
 
