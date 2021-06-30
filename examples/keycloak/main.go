9d55ebd18acfdac15ae2ecfed15741b8c2202f4e
Micah Parks <git@micahparks.com>
2021-06-30 14:49:51 -0400

    Updating references in code from legacy to new.

--- examples/keycloak/main.go
+++ examples/keycloak/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt"
 
 	"github.com/MicahParks/keyfunc"
 )
--- examples/recommended_options/main.go
+++ examples/recommended_options/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt"
 
 	"github.com/MicahParks/keyfunc"
 )
