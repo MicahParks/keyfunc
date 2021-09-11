48a9dd9127d7ac27dbd08397d87fa72e1af7fdf8
Micah Parks <git@micahparks.com>
2021-09-10 22:14:44 -0400

    Examples.

--- examples/aws_cognito/main.go
+++ examples/aws_cognito/main.go
@@ -47,8 +47,8 @@ func main() {
 	jwtB64 := "eyJraWQiOiJmNTVkOWE0ZSIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJLZXNoYSIsImF1ZCI6IlRhc2h1YW4iLCJpc3MiOiJqd2tzLXNlcnZpY2UuYXBwc3BvdC5jb20iLCJleHAiOjE2MTkwMjUyMTEsImlhdCI6MTYxOTAyNTE3NywianRpIjoiMWY3MTgwNzAtZTBiOC00OGNmLTlmMDItMGE1M2ZiZWNhYWQwIn0.vetsI8W0c4Z-bs2YCVcPb9HsBm1BrMhxTBSQto1koG_lV-2nHwksz8vMuk7J7Q1sMa7WUkXxgthqu9RGVgtGO2xor6Ub0WBhZfIlFeaRGd6ZZKiapb-ASNK7EyRIeX20htRf9MzFGwpWjtrS5NIGvn1a7_x9WcXU9hlnkXaAWBTUJ2H73UbjDdVtlKFZGWM5VGANY4VG7gSMaJqCIKMxRPn2jnYbvPIYz81sjjbd-sc2-ePRjso7Rk6s382YdOm-lDUDl2APE-gqkLWdOJcj68fc6EBIociradX_ADytj-JYEI6v0-zI-8jSckYIGTUF5wjamcDfF5qyKpjsmdrZJA"
 
 	// Parse the JWT.
-	token, err := jwt.Parse(jwtB64, jwks.Keyfunc)
-	if err != nil {
+	var token *jwt.Token
+	if token, err = jwt.Parse(jwtB64, jwks.Keyfunc); err != nil {
 		log.Fatalf("Failed to parse the JWT.\nError: %s", err.Error())
 	}
 
@@ -56,6 +56,8 @@ func main() {
 	if !token.Valid {
 		log.Fatalf("The token is not valid.")
 	}
-
 	log.Println("The token is valid.")
+
+	// End the background refresh goroutine when it's no longer needed.
+	jwks.EndBackground()
 }
