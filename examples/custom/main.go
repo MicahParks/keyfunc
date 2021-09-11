76dd4b17516cef6b76e74b004a9b3832d9b14c1b
Micah Parks <git@micahparks.com>
2021-09-10 22:41:46 -0400

    One example done.

--- examples/custom/main.go
+++ examples/custom/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 
@@ -12,45 +11,29 @@ import (
 
 func main() {
 
+	// Declare the custom signing method's key and key ID.
 	const key = ""
 	const exampleKID = "exampleKeyID"
 
+	// Register the custom signing method.
 	jwt.RegisterSigningMethod(method.CustomAlg, func() jwt.SigningMethod {
 		return method.EmptyCustom{}
 	})
 
-	jwks := keyfunc.NewGiven(map[string]keyfunc.GivenKey{
-		exampleKID: keyfunc.NewGivenCustom(method.EmptyCustom{}),
-	})
-
+	// Create and sign the token using the custom signing method.
 	unsignedToken := jwt.New(method.EmptyCustom{})
 	unsignedToken.Header["kid"] = exampleKID
-
 	jwtB64, err := unsignedToken.SignedString(key)
 	if err != nil {
 		log.Fatalf("Failed to self sign a custom token.\nError: %s.", err.Error())
 	}
 
-	// Create the keyfunc options. Use an error handler that logs. Timeout the initial JWKs refresh request after 10
-	// seconds. This timeout is also used to create the initial context.Context for keyfunc.Get.
-	refreshTimeout := time.Second * 10
-	options := keyfunc.Options{
-		RefreshTimeout: &refreshTimeout,
-		RefreshErrorHandler: func(err error) {
-			log.Printf("There was an error with the jwt.Keyfunc\nError: %s", err.Error())
-		},
-	}
+	// Create the JWKs from the given signing method's key.
+	jwks := keyfunc.NewGiven(map[string]keyfunc.GivenKey{
+		exampleKID: keyfunc.NewGivenCustom(key),
+	})
 
-	// Create the JWKs from the resource at the given URL.
-	jwks, err := keyfunc.Get(jwksURL, options)
-	if err != nil {
-		log.Fatalf("Failed to create JWKs from resource at the given URL.\nError: %s", err.Error())
-	}
-
-	// Get a JWT to parse.
-	jwtB64 := "eyJraWQiOiJmNTVkOWE0ZSIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJLZXNoYSIsImF1ZCI6IlRhc2h1YW4iLCJpc3MiOiJqd2tzLXNlcnZpY2UuYXBwc3BvdC5jb20iLCJleHAiOjE2MTkwMjUyMTEsImlhdCI6MTYxOTAyNTE3NywianRpIjoiMWY3MTgwNzAtZTBiOC00OGNmLTlmMDItMGE1M2ZiZWNhYWQwIn0.vetsI8W0c4Z-bs2YCVcPb9HsBm1BrMhxTBSQto1koG_lV-2nHwksz8vMuk7J7Q1sMa7WUkXxgthqu9RGVgtGO2xor6Ub0WBhZfIlFeaRGd6ZZKiapb-ASNK7EyRIeX20htRf9MzFGwpWjtrS5NIGvn1a7_x9WcXU9hlnkXaAWBTUJ2H73UbjDdVtlKFZGWM5VGANY4VG7gSMaJqCIKMxRPn2jnYbvPIYz81sjjbd-sc2-ePRjso7Rk6s382YdOm-lDUDl2APE-gqkLWdOJcj68fc6EBIociradX_ADytj-JYEI6v0-zI-8jSckYIGTUF5wjamcDfF5qyKpjsmdrZJA"
-
-	// Parse the JWT.
+	// Parse the token.
 	var token *jwt.Token
 	if token, err = jwt.Parse(jwtB64, jwks.Keyfunc); err != nil {
 		log.Fatalf("Failed to parse the JWT.\nError: %s", err.Error())
