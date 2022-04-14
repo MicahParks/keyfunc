ca430346ffd6ab549649860ebb9071d7802c33c9
Micah Parks <gpg@micahparks.com>
2022-04-14 10:40:32 -0400

    Update function comment

--- keyfunc.go
+++ keyfunc.go
@@ -14,8 +14,7 @@ var (
 	ErrKID = errors.New("the JWT has an invalid kid")
 )
 
-// Keyfunc is a compatibility function that matches the signature of github.com/golang-jwt/jwt/v4's jwt.Keyfunc
-// function.
+// Keyfunc matches the signature of github.com/golang-jwt/jwt/v4's jwt.Keyfunc function.
 func (j *JWKS) Keyfunc(token *jwt.Token) (interface{}, error) {
 	kidInter, ok := token.Header["kid"]
 	if !ok {
